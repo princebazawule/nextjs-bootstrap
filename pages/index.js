9ae83a5dbb489298629ea219045d6a1b34dc7523
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-15 16:07:18 -0400

    add bg image

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export const getStaticProps = async () => {
 
 export default function Home({ items }) {
   return (
-    <>
+  <>
       <Head>
         <title>PixlD - We build beautiful websites</title>
         <meta name="title" content="PixlD - We build beautiful websites" />
@@ -34,25 +34,41 @@ export default function Home({ items }) {
         <meta name="twitter:description" content="We are a boutique, web design & development firm based in sunny Barbados" />
       </Head>
       
-      <main className='position-relative min-vh-100 d-flex flex-column justify-content-center align-items-center bg-dark'>
+      <main 
+        className='position-relative min-vh-100 d-flex flex-column justify-content-center align-items-center bg bg-opacity-25 bg-dark'
+        style={{backgroundImage: `url('https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80')`, backgroundRepeat: `no-repeat`, backgroundSize: `cover`, height: '100vh'}}
+    >
+      
+
+      <h1 className='display-1 mb-4' style={{zIndex: `2000` }}>Who&apos;s PixlD?</h1>
+
+      {items && ( 
+        <div 
+          className='fs-3 fw-light mx-auto w-100 w-md-75 w-xxl-50 '
+          style={{zIndex: `2000` }}
+          dangerouslySetInnerHTML={{ __html: items.mini_profile}} ></div>
+      )}
+
+      <div 
+        className="get-in-touch mt-4"
+        style={{zIndex: `2000` }}
+      >
+        <Link href="/connect">
+          <a>
+            <button type="button" className="btn btn-primary btn-lg fs-3">get in touch</button>
+          </a>
+        </Link>
+      </div>
+
+      <div 
+        className='min-vh-100 min-vw-100 position-absolute'
+        style={{backgroundColor: `rgba(0, 0, 0, 0.8)`}}
+      ></div>
+
+      {/* <img 
+        src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80" 
+className='position-relative min-vh-100 d-flex flex-column justify-content-center align-items-center bg-img bg-dark bg-blend-multiply' /> */}
         
-
-        <h1 className='fs-1 fw-light mb-4'>Who&apos;s PixlD?</h1>
-
-        {items && ( 
-          <div 
-            className='fs-4 xl-mw-50 mx-auto' 
-            dangerouslySetInnerHTML={{ __html: items.mini_profile}} ></div>
-        )}
-
-        <div className="get-in-touch mt-4">
-          <Link href="/connect">
-            <a>
-              <button type="button" className="btn btn-primary btn-lg fs-3">get in touch</button>
-            </a>
-          </Link>
-        </div>
       </main>
-    </>
-  )
+    </>)
 }
