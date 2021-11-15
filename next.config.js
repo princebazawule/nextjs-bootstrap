28de3802040633bd26fcbefe3fab8190524dfcca
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-15 16:06:00 -0400

    configure sass

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const path = require('path');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+  cssModules: true
+})
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['pixldinc.link', 'https://pixldinc.link', 'images.unsplash.com', 'https://images.unsplash.com'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
